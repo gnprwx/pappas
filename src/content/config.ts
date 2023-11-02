88ae6662c341c228e7fad28145b59100865fea19
Greg Pappas <gnpappas@proton.me>
2023-11-02 07:44:07 -1000

    Init commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,9 +4,9 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string().optional(),
+		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date().optional(),
+		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
