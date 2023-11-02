36e52759ce4d7cefb686d3c76b4d54098fe19c71
Greg Pappas <gnpappas@proton.me>
2023-11-02 07:43:20 -1000

    Init commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'pappas.sh';
-export const SITE_DESCRIPTION = 'becoming a programmer';
+export const SITE_TITLE = "pappas.sh";
+export const SITE_DESCRIPTION = "just full-stack things";
