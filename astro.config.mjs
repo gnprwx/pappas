f9f0bcca48691465686332f371c128c33c8e7041
Greg Pappas <gnpappas@proton.me>
2023-12-27 06:38:25 -1000

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://pappas.sh",
-    integrations: [mdx(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-            wrap: true,
-        },
-    },
+  site: "https://pappas.sh",
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true
+    }
+  }
 });
