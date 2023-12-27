ecdfe5f09019c3c3dc1cf39b4ddd54d5de1a4280
Greg Pappas <gnpappas@proton.me>
2023-12-27 11:51:10 -1000

    Add frequent styles as consts

---
+++ src/styles/style.ts
@@ -0,0 +1,12 @@
+export const ROOT = "bg-zinc-900 font-mono sm:pb-0 pb-6";
+export const BASE = "mx-auto max-w-5xl text-green-300 sm:pb-20 pb-6 px-2";
+export const TITLE =
+  "text-center sm:text-6xl text-4xl font-bold text-green-300 m-2";
+export const SUBTITLE = "text-4xl font-bold";
+export const PROJECT_TITLE = "text-3xl font-bold text-green-200 underline";
+export const SUBJECT = "text-3xl font-bold text-green-200"
+export const LINE = "border-b-2 border-green-300/50 mb-2";
+export const PARA =
+  "text-emerald-200 bg-gradient-to-t from-green-950/40 to-transparent rounded border border-green-300/20 shadow-[0_0_5px_rgba(0,128,0,0.40)] mb-4 px-4 py-2";
+export const LINK = "font-bold underline";
+export const MDX = "mx-auto prose prose-invert sm:prose-lg text-green-200";
